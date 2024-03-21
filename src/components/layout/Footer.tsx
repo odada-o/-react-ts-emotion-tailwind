1695e9bc03f298dae2cc69448d729a2206d64935
odd:odd <eehd80yul@gmail.com>
2024-03-21 12:05:54 +0900

    router

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const Footer: React.FC = () => (
     <footer className="py-5">
-        <a href="#" className="-m-1.5 p-1.5">
+        <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img className="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-        </a>
+        </Link>
 
         <div className="font-normal text-center text-gray-900">© 2024 Tailwind Labs Inc. All rights reserved.</div>
 
